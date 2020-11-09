2ada0f9decb109e08c6c872173dd7de0ebddd182
Donald <donaldcela.fti@gmail.com>
2020-11-09 02:49:29 +0100

    Initial Commit

---
+++ CollectionList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionList
+//
+//  Created by Florian Cela on 9.11.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
